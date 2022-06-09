cea637a9c5d3e4303ada2689939e99b08738df5b
Victoria LeBel <vickileb@hotmail.com>
2022-06-08 17:00:21 -0700

    commit tree 76b9ec933d184f11d1dca2f6778086a0b56763dc

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const newP = document.getElementById('text')
+    newP.textContent = "This is really cool!"
+});
+
